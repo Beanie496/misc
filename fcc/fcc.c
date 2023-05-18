d9e999d11f12d31679ff00d6fcc62ed131f97dc7
Jasper Shovelton <Beanie.github@proton.me>
2023-05-18 21:34:37 +0100

    add new project

---
+++ fcc/fcc.c
@@ -0,0 +1,14 @@
+/* lc - print the number of letters in files
+ *
+ * This program comes without ANY license or warranty, including any implied
+ * warranty. You are free to copy, modify and redistribute it as you see fit.
+ */
+
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Hello world!\n");
+	return 0;
+}
