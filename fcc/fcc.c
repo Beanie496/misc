ad4233ab386cd60e096650bab485535172f8ca60
Jasper Shovelton <Beanie.github@proton.me>
2023-05-19 01:00:35 +0100

    add primative first implementation

--- fcc/fcc.c
+++ fcc/fcc.c
@@ -7,8 +7,53 @@
 #include <stdio.h>
 
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("Hello world!\n");
+	int count = 0;
+	FILE *file;
+
+	// TODO: handle command-line args
+	while (*(++argv)[0] == '-')
+		while (*++*argv)
+			switch (**argv) {
+				case 'a':
+					break;
+				case 'c':
+					break;
+				case 'f':
+					break;
+				case 'h':
+					break;
+				case 'l':
+					break;
+				case 'n':
+					break;
+				case 's':
+					break;
+				case 'v':
+					break;
+				default:
+					break;
+			}
+
+	// TODO: add custom args for each file
+	argv--;
+	while (*++argv) {
+		int currLetterCount = 0;
+		file = fopen(*argv, "r");
+		if (file == 0) {
+			fprintf(stderr, "Could not open file \"%s\"\n", *argv);
+			continue;
+		}
+		while (fgetc(file) != EOF) {
+			currLetterCount++;
+		}
+		count += currLetterCount;
+		printf("Count for this file: %d.\t\t Count total: %d\n",
+				currLetterCount, count);
+	}
+
+	printf("Total count: %d\n", count);
+
 	return 0;
 }
