d401521095f3b34905fb557fa2709a9520d91499
Jasper Shovelton <Beanie.github@proton.me>
2023-06-25 00:44:52 +0100

    fix logical errors

--- base64/base64cpy.c
+++ base64/base64cpy.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define u8 uint8_t
-
 char *encode(FILE *stream);
 char base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
@@ -21,13 +19,13 @@ int main(int argc, char *argv[])
 
 char *encode(FILE *stream)
 {
-	u8 nextByte = 0;
+	char nextByte = 0;
 	int bitMask = 0;
-	int length = 0;
-	char *encoded = malloc(0);
+	int idx = 0;
+	// 1 for the null char at the end
+	char *encoded = malloc(idx + 1);
 	// until EOF
-	while (1)
-	{
+	while (nextByte != EOF) {
 		/* from each triplet of bytes, these bits are fetched:
 		 * 0000 0000 0000 0000 0000 0000
 		 * `-----`
@@ -37,22 +35,35 @@ char *encode(FILE *stream)
 		 * and repeat
 		 */
 
-		length += 4;
-		encoded = realloc(encoded, length * sizeof(char));
+		encoded = realloc(encoded, (idx + 4) * sizeof(char));
 
 		nextByte = fgetc(stream);
+		if (nextByte == EOF)
+			continue;
+		// get the first 6 bits of nextByte, which is also all but the
+		// last 2 bits
 		bitMask = nextByte >> 2;
-		encoded[length - 4] = base64[bitMask];
+		encoded[idx++] = base64[bitMask];
+
 		nextByte = fgetc(stream);
-		bitMask <<= 4;
-		bitMask &= nextByte >> 4;
-		encoded[length - 3] = base64[bitMask];
+		if (nextByte == EOF)
+			continue;
+		// get first 2 bits and make them bits 5 and 6
+		bitMask = (bitMask & 0x3) << 4;
+		bitMask |= nextByte >> 4;
+		encoded[idx++] = base64[bitMask];
+
+		// get bits 0-3 of nextByte and make them bits 2-5
 		bitMask = (nextByte & 0xf) << 2;
 		nextByte = fgetc(stream);
+		if (nextByte == EOF)
+			continue;
+		// get last 2 bits of nextByte and make then the first 2
 		bitMask &= nextByte >> 6;
-		encoded[length - 2] = base64[bitMask];
+		encoded[idx++] = base64[bitMask];
+		// get the first 6 bits of nextByte
 		bitMask = nextByte & 0x3f;
-		encoded[length - 1] = base64[bitMask];
+		encoded[idx++] = base64[bitMask];
 	}
 
 	return encoded;
